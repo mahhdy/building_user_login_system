6ad682f95eb048b608ff13e318816c1be90ef8a4
Robson SIlva <robsonsilv410@gmail.com>
2017-11-20 13:19:48 -0300

    Changed on newer versions

--- finish/app.py
+++ finish/app.py
@@ -10,7 +10,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/login-example/database.db'
-Bootstrap(app)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
